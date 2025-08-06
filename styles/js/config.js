f3ecf365c78155582823935cfd9a382682297ca8
skulleids <skulleids@gmail.com>
2025-08-05 22:47:08 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -44,10 +44,10 @@ charadex.sheet = {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
+    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Ultra Rare', 'Legendary'],
+    species: ['All', 'Skulleid','Merskullies', 'Sugar Skullies', 'Plant Skullies'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
-    traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
+    traitTypes: ['All', 'Horns', 'Eyes', 'Bones', 'Skin', 'Ears', 'Extra', 'Mutations']
 
   }
 
